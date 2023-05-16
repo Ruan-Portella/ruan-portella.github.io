d27de1e5d9aea7018154be9e6a788a1a3f2b13ea
Ruan-Portella <ruanmorales29@gmail.br>
2023-05-16 19:35:12 -0300

    Feat: Adicionando Pastas Novas e Importando Libs

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-function App() {
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Home from './pages/Home';
 
+function App() {
+  const location = useLocation();
   return (
-    <>
-    </>
+    <div className="App">
+        <Routes location={location} key={location.pathname}>
+        <Route exact path='/' element={ <Home /> }/>
+        </Routes>
+    </div>
   )
 }
 
-export default App
+export default App;
+
