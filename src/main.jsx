b81528344bfd601fbe3311ae56e6603510125d6e
Ruan-Portella <ruanmorales29@gmail.br>
2023-05-17 17:43:13 -0300

    Feat: Implementa o context na main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom";
+import LanguageProvider from './context/LanguageProvider.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <LanguageProvider>
+       <App />
+    </LanguageProvider>
   </BrowserRouter>
 )
