fac844c4eedea505aed0669eb859f34a8d9fafa7
Ruan-Portella <ruanmorales29@gmail.br>
2023-05-18 16:33:14 -0300

    Feat: Cria componente de card para os projetos

---
+++ src/components/ProjectsCard.jsx
@@ -0,0 +1,41 @@
+import { Col } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types';
+import '../styles/ProjectsCard.css'
+import LanguageContext from "../context/LanguageContext";
+import { useContext } from 'react';
+import {motion} from 'framer-motion'
+
+export const ProjectCards = ({ title, imgUrl, stacks, id }) => {
+  const {isBR} = useContext(LanguageContext);
+  const transition = { duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96] };
+
+
+  return (
+    <Col sm={6} md={4}>
+      <div className='proj-imgbx'>
+        <img src={imgUrl}></img>
+        <motion.div exit={{ opacity: 0 }} transition={transition} className='proj-txtx'>
+          <h4>{title}</h4>
+          <section className='proj-span'>
+            <ul>
+              {
+                stacks.map((stack) => (
+                  <li key={stack.name}>{stack.image} <span>{stack.name}</span></li>
+                ))
+              }
+            </ul>
+          </section>
+          <button><Link to={`/details/${id}`}>{isBR ? 'Conheça o projeto' : 'Know the project'}</Link></button>
+        </motion.div>
+      </div>
+    </Col>
+  )
+}
+
+ProjectCards.propTypes = {
+  title: PropTypes.string.isRequired,
+  imgUrl: PropTypes.string.isRequired,
+  stacks: PropTypes.arrayOf(PropTypes.shape()),
+  id: PropTypes.number.isRequired,
+};
