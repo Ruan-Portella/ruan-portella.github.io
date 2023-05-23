32ab8a6a85b49d68f9e57be80341fc895368260a
Ruan-Portella <ruanmorales29@gmail.br>
2023-05-22 23:02:08 -0300

    Feat: Adiciona Dropdown para selecionar linguagem

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,11 @@
-import { useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import { Navbar, Nav, Container, Offcanvas } from "react-bootstrap";
 import { SiGithub } from 'react-icons/si';
 import { FaLinkedinIn } from 'react-icons/fa';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 import { US, BR } from 'country-flag-icons/react/3x2'
-import {CiLight, CiDark} from 'react-icons/ci'
+import { CiLight, CiDark } from 'react-icons/ci'
 import { useContext } from "react";
 import '../styles/NavBar.css'
 import PageContext from "../context/PageContext";
@@ -13,7 +15,7 @@ export default function NavBar(details) {
   const [scrolled, setScrolled] = useState(false);
   const [activeLink, setActiveLink] = useState('home');
   const [isToggler, setIsToggler] = useState(false);
-  const {isBR, handleChangeLanguage, isDark, handleChangeColor} = useContext(PageContext);
+  const { isBR, handleChangeLanguage, isDark, handleChangeColor } = useContext(PageContext);
 
 
   useEffect(() => {
@@ -43,96 +45,108 @@ export default function NavBar(details) {
           <p className="brandLogo">RP</p>
         </Navbar.Brand>
         {
-          details.details ? (<Nav  className="ms-auto"><Nav.Link className="navbar-link"  onClick={() => {
+          details.details ? (<Nav className="ms-auto"><Nav.Link className="navbar-link" onClick={() => {
             window.location.href = '/#projects'
-    }}>{isBR ? 'Voltar' : 'Return'}</Nav.Link></Nav>) : (
-      <>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" className={isToggler ? 'hamburger-x' : ''} onClick={() => isToggler ? setIsToggler(false) : setIsToggler(true)}>
-      <span className="hamburger-icon"></span>
-    </Navbar.Toggle>
-      <Navbar.Offcanvas
-      className={isDark ? 'offcanvas_dark' : 'offcanvas_light'}
-      id={`offcanvasNavbar-expand-lg`}
-      aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
-      placement="end">
-      <Offcanvas.Body>
-          <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ms-auto">
-                  <Nav.Link
-                      href="#home"
-                      className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'}
-                      onClick={() => {
+          }}>{isBR ? 'Voltar' : 'Return'}</Nav.Link></Nav>) : (
+            <>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" className={isToggler ? 'hamburger-x' : ''} onClick={() => isToggler ? setIsToggler(false) : setIsToggler(true)}>
+                <span className="hamburger-icon"></span>
+              </Navbar.Toggle>
+              <Navbar.Offcanvas
+                className={isDark ? 'offcanvas_dark' : 'offcanvas_light'}
+                id={`offcanvasNavbar-expand-lg`}
+                aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
+                placement="end">
+                <Offcanvas.Body>
+                  <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                      <Nav.Link
+                        href="#home"
+                        className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'}
+                        onClick={() => {
                           setTimeout(() => {
                             setActiveLink('home');
-                              window.location.href = '#home'
+                            window.location.href = '#home'
                           }, 400);
                           return false;
-                      }}
-                  >
-                      {isBR ? 'Inicio' : 'Home'}
-                  </Nav.Link>
-                  <Nav.Link href="#skills"
-                      className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'}
-                      onClick={() => {
+                        }}
+                      >
+                        {isBR ? 'Inicio' : 'Home'}
+                      </Nav.Link>
+                      <Nav.Link href="#skills"
+                        className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'}
+                        onClick={() => {
                           setTimeout(() => {
                             setActiveLink('skills');
-                              window.location.href = '#skills';
+                            window.location.href = '#skills';
                           }, 400);
                           return false;
-                      }}
-                  >
-                      {isBR ? 'Habilidades' : 'Skills'}
-                  </Nav.Link>
-                  <Nav.Link href="#projects"
-                      className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'}
-                      onClick={() => {
+                        }}
+                      >
+                        {isBR ? 'Habilidades' : 'Skills'}
+                      </Nav.Link>
+                      <Nav.Link href="#projects"
+                        className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'}
+                        onClick={() => {
                           setTimeout(() => {
                             setActiveLink('projects');
-                              window.location.href = '#projects';
+                            window.location.href = '#projects';
                           }, 400);
                           return false;
-                      }}
-                  >
-                      {isBR ? 'Projetos' : 'Projects'}
-                  </Nav.Link>
-                  <Nav.Link href="#contate-me"
-                      className={activeLink === 'contate-me' ? 'active navbar-link' : 'navbar-link'}
-                      onClick={() => {
+                        }}
+                      >
+                        {isBR ? 'Projetos' : 'Projects'}
+                      </Nav.Link>
+                      <Nav.Link href="#contate-me"
+                        className={activeLink === 'contate-me' ? 'active navbar-link' : 'navbar-link'}
+                        onClick={() => {
                           setTimeout(() => {
                             setActiveLink('contate-me');
-                              window.location.href = '#contate-me';
+                            window.location.href = '#contate-me';
                           }, 500);
                           return false;
-                      }}
-                  >
-                      {isBR ? 'Contate-me' : 'Contact Me'}
-                  </Nav.Link>
-                  </Nav>
-              <span className="navbar-text">
-                  <div className="social-icon">
-                      <a href="https://github.com/Ruan-Portella" target='_blank' rel="noreferrer"><SiGithub className="image" /></a>
-                      <a href="https://www.linkedin.com/in/ruanportella/" target='_blank' rel="noreferrer"><FaLinkedinIn className="image" /></a>
-                      {
-                        isBR ? (
-                          <button className="buttonFlag" onClick={() => handleChangeLanguage(false)}><US className="image" /></button>
-                        ) : (
-                          <button className="buttonFlag" onClick={() => handleChangeLanguage(true)}><BR className="image" /></button>
-                        )
-                      }
-                      {
-                        isDark ? (
-                          <button className="buttonColor" onClick={() => handleChangeColor(false)} ><CiLight className="image" /></button>
-                        ) : (
-                          <button className="buttonColor" onClick={() => handleChangeColor(true)}><CiDark className="image" /></button>
-                        )
-                      }
-                  </div>
-              </span>
-          </Navbar.Collapse>
-      </Offcanvas.Body>
-  </Navbar.Offcanvas>
-  </>
-    )
+                        }}
+                      >
+                        {isBR ? 'Contate-me' : 'Contact Me'}
+                      </Nav.Link>
+                    </Nav>
+                    <span className="navbar-text">
+                      <div className="social-icon">
+                        <a href="https://github.com/Ruan-Portella" className="iconSocial" target='_blank' rel="noreferrer"><SiGithub className="image" /></a>
+                        <a href="https://www.linkedin.com/in/ruanportella/" className="iconSocial" target='_blank' rel="noreferrer"><FaLinkedinIn className="image" /></a>
+                        {
+                          isDark ? (
+                            <button className="buttonColor" onClick={() => handleChangeColor(false)} ><CiLight className="image" /></button>
+                          ) : (
+                            <button className="buttonColor" onClick={() => handleChangeColor(true)}><CiDark className="image" /></button>
+                          )
+                        }
+                        <DropdownButton id="dropdown-basic-button" title={isBR ? (
+                        <>
+                        <BR className="image" />
+                        <span>BR</span>
+                        </>) : (
+                        <>
+                        <US className="image" />
+                        <span>EN</span>
+                        </>
+                        )}>
+                          <Dropdown.Item onClick={ () => handleChangeLanguage(false)}>
+                          <US className="image" />
+                            <span>English</span>
+                            </Dropdown.Item>
+                          <Dropdown.Item onClick={ () => handleChangeLanguage(true)} >
+                            <BR className="image" />
+                            <span>Português</span>
+                            </Dropdown.Item>
+                        </DropdownButton>
+                      </div>
+                    </span>
+                  </Navbar.Collapse>
+                </Offcanvas.Body>
+              </Navbar.Offcanvas>
+            </>
+          )
         }
       </Container>
     </Navbar>
