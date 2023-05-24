22fcf0a73d9b06d79dfdd8e31cf8fba04844dd14
Ruan-Portella <ruanmorales29@gmail.br>
2023-05-24 17:39:59 -0300

    Feat: Adiciona logica para mostrar de 6 em 6 projetos

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,4 +1,4 @@
-import { Col, Container, Nav, Row, Tab } from 'react-bootstrap'
+import { Button, Col, Container, Nav, Row, Tab } from 'react-bootstrap'
 import { projects } from '../utils/projectData';
 import { ProjectCards } from '../components/ProjectsCard'
 import { motion } from 'framer-motion';
@@ -6,10 +6,12 @@ import PageContext from "../context/PageContext";
 import { useEffect } from 'react';
 import { useContext } from 'react';
 import '../styles/Projects.css'
+import { useState } from 'react';
 
 export function Projects() {
-    const {isBR} = useContext(PageContext);
-  const transition = { duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96] };
+    const [visibleCount, setVisibleCount] = useState(6);
+    const { isBR, isDark } = useContext(PageContext);
+    const transition = { duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96] };
 
 
 
@@ -30,57 +32,74 @@ export function Projects() {
         <motion.section exit={{ opacity: 0 }} transition={transition} className='project' id='projects'>
             <Container>
                 <motion.div
-                    whileInView={{ opacity: 1, x: 0, rotate: 0}}
-                    initial={{ opacity: 0, x: 200, rotate: 0}}
+                    whileInView={{ opacity: 1, x: 0, rotate: 0 }}
+                    initial={{ opacity: 0, x: 200, rotate: 0 }}
                     transition={{ duration: 0.7 }}
                     viewport={{ once: false }}>
                     <Row>
                         <Col>
                             <h2>{isBR ? 'Projetos' : 'Projects'}</h2>
                             <p>{isBR ? 'No decorrer da minha trajetória como dev, desenvolvi as seguintes habilidades:' : "During my career as a dev, I developed the following skills:"}</p>
-                            <Tab.Container id='projects-tabs' defaultActiveKey="front-end">
+                            <Tab.Container id='projects-tabs' defaultActiveKey="all">
                                 <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                                     <Nav.Item>
-                                        <Nav.Link eventKey="all">{isBR ? 'Todos' : 'All'}</Nav.Link>
+                                        <Nav.Link eventKey="all" onClick={() => setVisibleCount(6)} >{isBR ? 'Todos' : 'All'}</Nav.Link>
                                     </Nav.Item>
                                     <Nav.Item>
-                                        <Nav.Link eventKey="front-end">Front-End</Nav.Link>
+                                        <Nav.Link eventKey="front-end" onClick={() => setVisibleCount(6)}>Front-End</Nav.Link>
                                     </Nav.Item>
                                     <Nav.Item>
-                                        <Nav.Link eventKey="back-end">Back-End</Nav.Link>
+                                        <Nav.Link eventKey="back-end" onClick={() => setVisibleCount(6)}>Back-End</Nav.Link>
                                     </Nav.Item>
                                 </Nav>
                                 <Tab.Content>
                                     <Tab.Pane eventKey='all'>
                                         <Row>
                                             {
-                                                projects.map((project, index) => {
+                                                projects.slice(0, visibleCount).map((project, index) => {
                                                     return (
                                                         <ProjectCards key={index} {...project}></ProjectCards>
                                                     )
                                                 })
+                                            }
+                                            {
+                                                projects.length > visibleCount && (
+                                                    <div className='buttonProject'>
+                                                        <Button className={`visibleBtn ${isDark ? '' : 'visibleBtnLight'}`}  variant='dark' onClick={() => setVisibleCount(prevCount => prevCount + 6)} ><a>Ver mais</a></Button>
+                                                    </div>)
                                             }
                                         </Row>
                                     </Tab.Pane>
                                     <Tab.Pane eventKey='front-end'>
                                         <Row>
                                             {
-                                                projects.filter((project) => project.type === 'FrontEnd').map((project, index) => {
+                                                projects.filter((project) => project.type === 'FrontEnd').slice(0, visibleCount).map((project, index) => {
                                                     return (
                                                         <ProjectCards key={index} {...project}></ProjectCards>
                                                     )
                                                 })
+                                            }
+                                            {
+                                                projects.filter((project) => project.type === 'FrontEnd').length > visibleCount && (
+                                                    <div className='buttonProject'>
+                                                        <Button className={`visibleBtn ${isDark ? '' : 'visibleBtnLight'}`}  variant='dark' onClick={() => setVisibleCount(prevCount => prevCount + 6)} ><a>Ver mais</a></Button>
+                                                    </div>)
                                             }
                                         </Row>
                                     </Tab.Pane>
                                     <Tab.Pane eventKey='back-end'>
                                         <Row>
                                             {
-                                                projects.filter((project) => project.type === 'BackEnd').map((project, index) => {
+                                                projects.filter((project) => project.type === 'BackEnd').slice(0, visibleCount).map((project, index) => {
                                                     return (
                                                         <ProjectCards key={index} {...project}></ProjectCards>
                                                     )
                                                 })
+                                            }                                            {
+                                                projects.filter((project) => project.type === 'BackEnd').length > visibleCount && (
+                                                    <div className='buttonProject'>
+                                                        <Button className={`visibleBtn ${isDark ? '' : 'visibleBtnLight'}`}  variant='dark' onClick={() => setVisibleCount(prevCount => prevCount + 6)} ><a>Ver mais</a></Button>
+                                                    </div>)
                                             }
                                         </Row>
                                     </Tab.Pane>
