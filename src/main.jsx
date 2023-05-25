ba7ed7296ebf0b02938f0b1ec45cf64163497667
Ruan-Portella <ruanmorales29@gmail.br>
2023-05-24 23:07:49 -0300

    Feat: Muda de BrowserRouter para HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from 'react-router-dom/cjs/react-router-dom.min';
 import PageProvider from './context/PageProvider.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <PageProvider>
        <App />
     </PageProvider>
-  </BrowserRouter>
+  </HashRouter>
 )
